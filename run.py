e7e38beac16200e55cf39d4b651ed97b28f4e675
Bert Palm <bert.palm@ufz.de>
2023-03-15 21:40:02 +0100

    rm dead code from run.py

--- run.py
+++ run.py
@@ -1,53 +1,9 @@
 #!/usr/bin/env python
 from __future__ import annotations
 
-
 import pandas as pd
+import numpy as np
 from qaqc import Variable, QaqcFrame
 
 
-description = "Quality control for data series"
-if __name__ == "__main__":
-    s = pd.Series(range(5), dtype=float)
-    f = Variable(s)
-    r = f.mask(s >= 2).setAll(77).unmask().select(s > 2).setAll(66).deselect()
-    print(r, "\n\n")
 
-    print("###### with statement ####", "\n\n")
-
-    s = pd.Series(range(8), dtype=float)
-    g = Variable(s)
-    with g.selecting(s > 2) as masked:
-        masked = masked.setAll(11)
-        with masked.selecting(s >= 5) as masked_the_masked:
-            r = masked_the_masked.setAll(99)
-    print(r)
-
-if __name__ == "__main__":
-    n = 5
-    index = pd.date_range("2000", freq="1d", periods=n)
-    x = Variable(pd.Series(range(n), index=index, dtype=float))
-    b = x.flagUpper(3, flag=33)
-
-    qc = SaQC()
-    qc["a"] = x
-    qc["b"] = x.copy()
-    qc = qc.flagUpper(3)
-    qc = qc.flagFoo()
-    qc = qc.dododo(None, None)
-    qc.show()
-    qc = qc.dododo(None, None)
-    qc.show()
-
-    # pd.DataFrame.replace()
-    # pd.DataFrame.reindex
-    # pd.Series.reindex()
-
-    # a.history.current()
-    #
-    # print("a")
-    # print(a)
-    # print("a.data")
-    # print(a.data, end="\n\n")
-    # print("a.history")
-    # print(a.history)
